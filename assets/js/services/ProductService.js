74e1823e7b245d2936ef64534c1cbf72e0494e4f
Keyla <20133ti169@utez.edu.mx>
2022-11-19 22:16:54 -0600

    fix: fix products functions

--- assets/js/services/ProductService.js
+++ assets/js/services/ProductService.js
@@ -1,23 +1,23 @@
 import { API_URI, HEADERS_URI } from "./API.js";
-import { getToken} from "../utils/LocalStorage.js";
-import {goToPage} from "../utils/Routes.js";
+
+import { goToPage } from "../utils/Routes.js";
 import NotifyService from "../utils/NotifyService.js";
+const getUrl = new URLSearchParams(window.location.search);
+let id = getUrl.get('id');
 
-const contenedor = document.getElementById('page') || document.createElement('page');
+const contenedor =
+  document.getElementById("page");
 
-
-fetch(API_URI+'/products', {
-    
-    method: "GET",
-    headers: HEADERS_URI, 'Authorization': `Bearer 11| ${getToken()}`
-  })
-    .then(response => response.json())
-    .then(data =>mostrar(data))
-    const mostrar = (products) => {
-        
-        products.data.forEach(product => {
-console.log(product)
-            contenedor.innerHTML += `      <div class="col-xl-4 col-lg-6 col-md-6">
+fetch(API_URI + "/products", {
+  method: "GET",
+  headers: HEADERS_URI,
+})
+  .then((response) => response.json())
+  .then((data) => mostrar(data));
+const mostrar = (products) => {
+  products.data.forEach((product) => {
+    console.log(product);
+    contenedor.innerHTML += `<div class="col-xl-4 col-lg-6 col-md-6">
             <div class="card mb-3 bg-light">
                 <div class="row g-0">
                     <div
@@ -56,36 +56,19 @@ console.log(product)
             </div>
         </div>
       </div>
-            `
-                        
-           
-           
-        });
-        
-
-        
-        
+            `;
+  });
+};
+fetch(API_URI + "/products/" + id, {
+  method: "DELETE",
+  headers: HEADERS_URI,
+})
+  .then((respuesta) => respuesta.json())
+  .then((data) => {
+    if (data.success == true) {
+      goToPage("../../views/products/products.html");
+    } else {
+      NotifyService.notificatonError("No se borro correctamente!");
     }
-
-    
-        fetch(API_URI +'/products/'+id,
-                        {
-                            method: 'DELETE',
-                            headers:HEADERS_URI ,
-                            
-                        })
-                        .then(respuesta => respuesta.json())
-                        .then(data =>{
-                            if(data.success == true){
-                                
-                                goToPage('../../views/products/products.html');
-                        }else{
-                            NotifyService.notificatonError('No se borro correctamente!')
-                            
-                        }
-                    })
-                            
-                            
-                        .catch(error => alert(error))
-        
-                
+  })
+  .catch((error) => console.log(error));
