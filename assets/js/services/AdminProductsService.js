6b6b57451a93b68514d672f8dbe5122d1a3dc272
Jackeline Aguas <20193tn001@utez.edu.mx>
2022-12-01 12:46:32 -0600

    update functions of products

--- assets/js/services/AdminProductsService.js
+++ assets/js/services/AdminProductsService.js
@@ -1,60 +1,51 @@
 import { API_URI, HEADERS_URI } from "./API.js";
 import { goToPage } from "../utils/Routes.js";
-import ProductService from "./ProductService.js";
 import NotifyService from "../utils/NotifyService.js";
 
 const registerForm = document.querySelector('#registerProduct') || document.createElement('form');
 const inputName = document.querySelector('#name') || document.createElement('input');
 const inputPrice = document.querySelector('#price') || document.createElement('input');
 const inputDescription = document.querySelector('#description') || document.createElement('input');
-let fileField = ["https://static.vecteezy.com/system/resources/previews/008/289/394/non_2x/illustrations-chocolate-cake-free-vector.jpg"];
 
+let images = [
+    "https://cdn.sstatic.net/Img/teams/teams-illo-free-sidebar-promo.svg"];
 
-registerForm.addEventListener('submit', register);
+registerForm.addEventListener('submit', saveProduct);
 
+async function createProduct() {
+    fetch(API_URI+'/products', {
+      method: "POST",
+      headers: HEADERS_URI,
+      body: JSON.stringify({
+        name: inputName.value,
+        description: inputDescription.value,
+        public_price: inputPrice.value,
+        image: images,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        if (data.success === true) {
+          goToPage("../../../views/products/products.html");
+          NotifyService.notificatonSuccess('Producto registrado correctamente');
+        } else {
+          NotifyService.notificatonError('Error al registrar el producto');
+        }
+      });
+  }
 
-fetch(API_URI + `/products`, {
-    method: "GET",
-    headers: HEADERS_URI,
-  })
-  .then((response) => response.json())
-  .then((data)=>{
-    console.log(data);
-  })
+  
+function validateForm(){
+  return(
+      inputName.value !== "" &&
+      inputPrice.value !== "" &&
+      inputDescription.value !== "" &&
+      images.length !== 0
+  );}
 
-
-function validInputs() {
-    return inputName.value !== '' || inputPrice.value !== '' || inputDescription.value !== ''
-}
-
-
-function sendProductRequest() {
-    NotifyService.loadingNotification()
-    ProductService.RegisterProduct(inputName.value, inputPrice.value, inputDescription.value, fileField.value)
-        .then(response => response.json())
-        .then(data => {
-            if (data.success) {
-                console.log(data);
-                setData('product', JSON.stringify(product));
-                NotifyService.notificatonError('Producto Registrado')
-                NotifyService.loadingNotificationRemove()
-            } else {
-                NotifyService.notificatonError('Error al registrar la Tienda')
-                NotifyService.loadingNotificationRemove()
-            }
-            console.log('Error');
-        }).catch(error => {
-            NotifyService.notificatonError('Hubo un error en el servicio')
-            NotifyService.loadingNotificationRemove()
-            })
-}
-
-function register(e) {
-    e.preventDefault();
-    if (validInputs()) {
-        sendProductRequest();
-    } else {
-       // ToastifyService.notificatonError('Los campos no deben estar vacios');
-       alert('Los campos no pueden estar vacíos')
-    }
+function saveProduct(e) {
+  e.preventDefault();
+  if (validateForm()) createProduct();
+  else NotifyService.notificatonError("Los campos no deben estar vacíos");
 }
